557cb87a4c7138cd6a548f990d43f5c7e99065c5
viktroria <63126758+viktroria@users.noreply.github.com>
2023-03-03 10:15:59 +0200

    start project

---
+++ Waxom/js/script.js
@@ -0,0 +1,14 @@
+;(function dynamicsOfNumbers(){
+	$('.count').each(function () {
+		$(this).prop('Counter',0).animate({
+			Counter:$(this).text()
+			},{
+				duration: 4000,
+				easing: 'swing',
+				step:function(now) {
+					$(this).text(Math.ceil(now));
+			}
+		});
+	});
+	
+})();
